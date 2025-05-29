ca17e56d88148cb843e08e62f8ecb45ed09dbfeb
Maitreyi Sinha <maitres@uci.edu>
2025-05-29 11:30:50 -0700

    uncomment build index, need to rebuild

--- index.py
+++ index.py
@@ -6,5 +6,5 @@ from index_builder import build_index
 from search import search_interface
 
 if __name__ == "__main__":
-    # build_index() # Uncomment to build index
+    build_index() # Uncomment to build index
     search_interface()
